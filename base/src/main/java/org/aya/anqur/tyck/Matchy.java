acacf9aaa358f5c9a80069f93d0154019791af2a
ice1000 <ice1000kotlin@foxmail.com>
2022-09-07 22:09:43 -0400

    pat: tyck clauses

--- base/src/main/java/org/aya/anqur/tyck/Matchy.java
+++ base/src/main/java/org/aya/anqur/tyck/Matchy.java
@@ -1,10 +1,21 @@
 package org.aya.anqur.tyck;
 
+import kala.collection.immutable.ImmutableSeq;
+import org.aya.anqur.syntax.Expr;
 import org.aya.anqur.syntax.Pat;
 import org.aya.anqur.syntax.Term;
+import org.aya.anqur.util.Param;
 import org.jetbrains.annotations.NotNull;
 
 public record Matchy(@NotNull Elaborator elaborator) {
+  public @NotNull Pat.Clause<Term> clause(
+    @NotNull ImmutableSeq<Param<Term>> params,
+    @NotNull Term result,
+    @NotNull Pat.Clause<Expr> clause
+  ) {
+    pats(params, clause.pats());
+    return new Pat.Clause<>(clause.pats(), elaborator.inherit(clause.body(), result));
+  }
   public void pat(@NotNull Pat pat, @NotNull Term type) {
     switch (pat) {
       case Pat.Bind bind -> elaborator.gamma().put(bind.bind(), type);
@@ -12,11 +23,14 @@ public record Matchy(@NotNull Elaborator elaborator) {
         var core = con.ref().core;
         var expected = core.owner();
         if (data.fn() != expected) throw new RuntimeException("Expected " + data.fn().name + ", got " + expected.name);
-        core.tele().zipView(con.pats())
-          .forEach(pair -> pat(pair._2, pair._1.type()));
+        pats(core.tele(), con.pats());
       }
       case Pat.Con con -> throw new RuntimeException(
         "So " + con.ref().name + " is not a constructor for type " + type.toDoc().commonRender());
     }
+  }
+
+  private void pats(ImmutableSeq<Param<Term>> tele, ImmutableSeq<Pat> pats) {
+    tele.zipView(pats).forEach(pair -> pat(pair._2, pair._1.type()));
   }
 }
